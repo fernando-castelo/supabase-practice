e0be8e70af52749d4d2ab5127a1ff97b67081fc8
fernando-castelo <nando.castelobranco24@hotmail.com>
2024-03-18 10:00:08 -0300

    refactor(): remove unused function

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,27 +2,9 @@ import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Supabase } from './common/supabase';
 import { CreateUserDto } from './interface/CreateUserDto';
 
-type Hello = {
-  id: number;
-  name: string;
-  description;
-};
-
 @Injectable()
 export class AppService {
   constructor(private readonly supabase: Supabase) {}
-
-  async getHello(): Promise<Hello[]> {
-    const { data, error } = await this.supabase
-      .getClient()
-      .from('user')
-      .select();
-    if (error) {
-      throw new InternalServerErrorException(error.message);
-    }
-
-    return data;
-  }
 
   async createUser(user: CreateUserDto) {
     const { data, error } = await this.supabase
